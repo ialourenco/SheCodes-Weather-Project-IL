0d7149c53176ae868a9482b03f2632cf89cc26b7
ialourenco <inesandreialourenco@gmail.com>
2020-06-14 22:16:12 +0100

    format forecast

--- SRC/script.js
+++ SRC/script.js
@@ -105,21 +105,22 @@ findLocation.addEventListener("click", getCurrentLocation);
 //Weather and other info
 function showForecast(response) {
   let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
   let forecast = null;
   console.log(response.data);
 
   for (let index = 0; index < 6; index++) {
     forecast = response.data.list[index];
     forecastElement.innerHTML += `
-    <div class="col-2">
+    <div class="col-2" id="forecastText">
       <h6>${formatHours(forecast.dt * 1000)}</h6>
-      <img
+      <img id = "forecastIcons"
         src="https://openweathermap.org/img/wn/${
           forecast.weather[0].icon
         }@2x.png"
       />
       <div class="weather-forecast-temperature">
-        <strong>${Math.round(forecast.main.temp_max)}°</strong>
+        <strong>${Math.round(forecast.main.temp_max)}°</strong>  
         ${Math.round(forecast.main.temp_min)}°
       </div>
     </div>
